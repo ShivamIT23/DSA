7ddcf9e0759c96b6ce91204009699a74ba149177
ShivamIT23 <shivam.gupta.23112003@gmail.com>
2024-04-29 10:31:03 +0530

    Initializing Files

--- Day_3/code.cpp
+++ Day_3/code.cpp
@@ -4,5 +4,8 @@ using namespace std;
 int main(){
     cout<<(10/3.0)<<endl;
     cout<<('a'+1)<<endl;
+    cout<<(double)(10/3)<<endl;
+    cout<<(char)('a'+1)<<endl;
+    cout<<(bool)('a')+1<<endl;
     return 0;
 }
