771d97110036a3a54b537b9fa26483239c1dbba3
ShivamIT23 <shivam.gupta.23112003@gmail.com>
2024-04-29 10:49:22 +0530

    Ending Files

---
+++ Day_3/operator.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    //  Arithmetic Operators
+
+    int a = 3;
+    int b = 2;
+    /*cout << a + b << endl;
+    cout << a - b << endl;
+    cout << a * b << endl;
+    cout << a / b << endl;
+    cout << a % b << endl;
+    cout << a++ << endl;
+    cout << a-- << endl;
+    cout << ++a << endl;
+    cout << --a << endl;
+    cout << ++b << endl;
+    cout << --b << endl;
+    cout << b++ << endl;
+    cout << b-- << endl;*/
+
+    /// Assignment Operator
+    /*int c = b;
+    cout<<c<<endl;
+    c+=3;
+    cout<<c<<endl;
+    c-=1;
+    cout<<c<<endl;
+    c*=2;
+    cout<<c<<endl;
+    c/=3;
+    cout<<c<<endl;*/
+
+    /// Comparison(Relational) Operator
+    /*cout << (a == b) << endl;
+    cout << (a != b) << endl;
+    cout << (a >= b) << endl;
+    cout << (a <= b) << endl;
+    cout << (a > b) << endl;
+    cout << (a < b) << endl;*/
+
+    /// Logical Operator
+    cout << (a>b && a<b) << endl;
+    cout << (a>b || a<b) << endl;
+    cout << (!a) << endl;
+
+    return 0;
+}
