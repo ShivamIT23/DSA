492c378d09c194056591246532f532b5019ffa38
ShivamIT23 <shivam.gupta.23112003@gmail.com>
2024-04-29 11:10:43 +0530

    adding Code

--- Day_4/code.cpp
+++ Day_4/code.cpp
@@ -3,15 +3,27 @@ using namespace std;
 
 int main()
 {
-    int a , b;
-    cout<< "Enter first No"<<endl;
-    cin>> a ;
-    cout<< "Enter second No"<<endl;
-    cin>> b ;
-    if(a>b){
-        cout<<"Largest No : "<<a<<endl;
-    }else{
-        cout<<"Largest No : "<<b<<endl;
+    int a, b;
+    cout << "Enter first No" << endl;
+    cin >> a;
+    cout << "Enter second No" << endl;
+    cin >> b;
+    if (a > b)
+    {
+        cout << "Largest No : " << a << endl;
+    }
+    else
+    {
+        cout << "Largest No : " << b << endl;
+    }
+
+    if (a % 2 == 0)
+    {
+        cout << a << " is even" << endl;
+    }
+    else
+    {
+        cout << a << " is odd" << endl;
     }
     return 0;
 }
