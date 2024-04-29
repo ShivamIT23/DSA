a922cb7a29ed5283e7a20269640b70f0bbda3832
ShivamIT23 <shivam.gupta.23112003@gmail.com>
2024-04-29 11:38:36 +0530

    Ending Files

--- Day_4/Calculator.cpp
+++ Day_4/Calculator.cpp
@@ -1,36 +1,38 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    int a , b;
+int main()
+{
+    int a, b;
     float result;
     char oper;
-    cout<<"Enter first no. : "<<endl;
-    cin>>a;
-    cout<<"Enter second no. : "<<endl;
-    cin>>b;
-    cout<<"Enter Operator : "<<endl;
-    cin>>oper;
+    cout << "Enter first no. : " << endl;
+    cin >> a;
+    cout << "Enter second no. : " << endl;
+    cin >> b;
+    cout << "Enter Operator : " << endl;
+    cin >> oper;
 
-    switch(oper) {
-        case '+':
-            result = a + b;
-            cout<<"Addition is : "<<result<<endl;
-            break;
-        case '-':
-            result = a - b;
-            cout<<"Subtraction is : "<<result<<endl;
-            break;
-        case '*':
-            result = a * b;
-            cout<<"Multiplication is : "<<result<<endl;
-            break;
-        case '/':
-            result = a / b;
-            cout<<"Division is : "<<result<<endl;
-            break;
-        default:
-            cout<<"Invalid Operator"<<endl;
+    switch (oper)
+    {
+    case '+':
+        result = a + b;
+        cout << "Addition is : " << result << endl;
+        break;
+    case '-':
+        result = a - b;
+        cout << "Subtraction is : " << result << endl;
+        break;
+    case '*':
+        result = a * b;
+        cout << "Multiplication is : " << result << endl;
+        break;
+    case '/':
+        result = a / b;
+        cout << "Division is : " << result << endl;
+        break;
+    default:
+        cout << "Invalid Operator" << endl;
     }
     return 0;
 }
