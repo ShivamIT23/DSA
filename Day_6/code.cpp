fc2100ce87301e6dceb4b32fa2d6992fa80c2965
ShivamIT23 <shivam.gupta.23112003@gmail.com>
2024-05-04 02:20:29 +0530

    adding Code

---
+++ Day_6/code.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            cout<<i+1<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
