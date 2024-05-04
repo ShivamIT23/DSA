9dad66518d371604f58ab58c0b46a395428cc574
ShivamIT23 <shivam.gupta.23112003@gmail.com>
2024-05-04 11:52:48 +0530

    adding Code

--- Day_6/code.cpp
+++ Day_6/code.cpp
@@ -3,15 +3,57 @@ using namespace std;
 
 int main()
 {
-    int n;
+    // int n;
 
-    for (int i = 0; i < 4; i++)
-    {
-        for (int j = 0; j < 4; j++)
-        {
-            cout<<i+1<<" ";
-        }
-        cout<<endl;
+    // for (int i = 0; i < 4; i++)
+    // {
+    //     for (int j = 0; j < 4; j++)
+    //     {
+    //         cout<<i+1<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    ///     Star Pattern
+
+    int n = 6;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < i+1; j++)
+    //     {
+    //         cout << "* ";
+    //     }
+    //     cout << endl;
+    // }
+    ///     Star Pattern - Inverted
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < n - i; j++)
+    //     {
+    //         cout << "* ";
+    //     }
+    //     cout << endl;
+    // }
+
+    ////    Half Pyramid Pattern  
+
+    // for(int i = 0 ; i<n;i++){
+    //     for(int j = 0; j<i+1;j++){
+    //         cout<<j+1;
+    // }
+    // cout<<endl;
+    // }
+
+    ////    Character INcrease Pyramid  
+
+    char ch = 'A';
+    for(int i = 0 ; i<n;i++){
+        for(int j = 0; j<i+1;j++){
+            cout<<ch++;
     }
+    cout<<endl;
+    }
+
     return 0;
 }
