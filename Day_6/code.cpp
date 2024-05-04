223ec63300740a04be015a88af9ef586c8fb5dc4
ShivamIT23 <shivam.gupta.23112003@gmail.com>
2024-05-04 12:18:39 +0530

    adding Code

--- Day_6/code.cpp
+++ Day_6/code.cpp
@@ -16,7 +16,7 @@ int main()
 
     ///     Star Pattern
 
-    int n = 6;
+    // int n = 6;
     // for (int i = 0; i < n; i++)
     // {
     //     for (int j = 0; j < i+1; j++)
@@ -47,12 +47,45 @@ int main()
 
     ////    Character INcrease Pyramid  
 
-    char ch = 'A';
-    for(int i = 0 ; i<n;i++){
-        for(int j = 0; j<i+1;j++){
-            cout<<ch++;
-    }
-    cout<<endl;
+    // char ch = 'A';
+    // for(int i = 0 ; i<n;i++){
+    //     for(int j = 0; j<i+1;j++){
+    //         cout<<ch++;
+    // }
+    // cout<<endl;
+    // }
+
+    ////    Hollow Star pattern
+
+    // int n = 33;
+
+    // for(int i =0;i<n;i++){
+    //     cout<<"* ";
+    //     for(int j=1;j<n-1;j++){
+    //         if(i==0 || i==n-1){
+    //             cout<<"* ";
+    //         }
+    //         else{
+    //             cout<<"  ";
+    //         }
+    //     }
+    //     cout<<"* ";
+    //     cout<<endl;
+    // }
+
+
+    ////    INverted STAR PYRAMID PATTERN  
+
+    int st = 4;
+    for(int i=0;i<st;i++){
+        for(int j=0;j<st;j++){
+            if(j<st-(i+1)){
+                cout<<" ";
+            }else{
+                cout<<"*";
+            }
+        }
+        cout<<endl;
     }
 
     return 0;
