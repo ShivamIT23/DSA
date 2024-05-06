b963409a878386385be473042735b8f499e60f9f
ShivamIT23 <shivam.gupta.23112003@gmail.com>
2024-05-06 06:02:33 +0530

    adding Code

---
+++ Assignment/Day_2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#define PI 3.14
+using namespace std;
+
+int main()
+{
+
+    /* In a program, input the side of a square. You have to output the area of the square.
+    Input : n (side) Output : n*n (area) */
+
+    // int n;
+    // cin >> n;
+    // cout << "Square of the Square is : " << n * n;
+
+    /* Enter cost of 3 items from the user (using float data type) - a pencil, a pen and an eraser. You have to output the total cost of the items back to the user as their bill.
+    (Add on : You can also try adding 18% GST tax to the items in the bill as an advanced problem) */
+
+    // float penPrize , pencilPrize , eraserPrize ;
+    // cout<<"Enter the prize of pencil : ";
+    // cin>>pencilPrize;
+    // cout<<"Enter the prize of pen : ";
+    // cin>>penPrize;
+    // cout<<"Enter the prize of eraser : ";
+    // cin>> eraserPrize;
+
+    // float total_cost = (pencilPrize + penPrize + eraserPrize);
+    // float cost_with_gst = (total_cost + total_cost*(18.0/100.0));
+
+    // cout<<"Total Cost : "<<total_cost<<endl;
+    // cout<<"With G.S.T. : " <<cost_with_gst<<endl;
+
+    ///     S.I. Calculator
+
+    // int p =100000;
+    // float r = 0.08;
+    // int t = 1;
+    // float si = p*r*t;
+    // cout<<"Simple Interest is : "<<si<<endl;
+
+    ///     Area of a circle
+
+    float r =5;
+    cout<<"Area of circle : "<<(PI *r*r); 
+    return 0;
+}
