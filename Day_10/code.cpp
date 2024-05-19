447e04e480d8c46ef57c76f469c1d8695d65fb8a
ShivamIT23 <shivam.gupta.23112003@gmail.com>
2024-05-19 16:05:59 +0530

    Adding code

--- Day_10/code.cpp
+++ Day_10/code.cpp
@@ -1,7 +1,20 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    int m;
+int main()
+{
+    int marks[50];
+    int marks2[59] = {1, 2, 3, 34521, 323, 45, 3545, 543, 54, 324, 534, 5432};
+
+    // cout<<marks[2]<<"\n";
+    // cout<<marks2[15]<<"\n";
+
+    int n = sizeof(marks) / sizeof(int);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << marks2[i] << " ";
+    }
+    cout << "\n";
     return 0;
 }
