fb6b02a10ccb158741ebf6990acc0b4951569d9c
ShivamIT23 <shivam.gupta.23112003@gmail.com>
2024-05-19 21:24:57 +0530

    Adding code

---
+++ Day_10/binarySearch.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int binSearch(int *arr, int n, int key)
+{
+    int st = 0, ed = n - 1;
+    while (st <= n) {
+        int mid = (st + ed) / 2;
+        if(arr[mid] == key){
+            return mid;
+        }
+        else if(arr[mid] < key){
+            st = mid + 1;
+        }
+        else{
+            ed = mid - 1;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    int arr[] = {2,4,6,7,8,9,12,14,16};
+    int key=7;
+    int n = sizeof(arr) / sizeof(int);
+
+    cout<<binSearch(arr,n,key)<<"\n";
+
+    return 0;
+}
