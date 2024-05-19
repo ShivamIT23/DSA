713c4eadadaac3dc1d87dd154064d0d9fa198225
ShivamIT23 <shivam.gupta.23112003@gmail.com>
2024-05-19 12:21:37 +0530

    Adding code

--- Day_9/code.cpp
+++ Day_9/code.cpp
@@ -1,13 +1,40 @@
 #include<iostream>
 using namespace std;
 
+void changeValA(int a){
+    a = 20;
+    cout<<a<<"\n";
+}
+
+void changePointerA(int *a){
+    *a = 20;
+    cout<<*a<<"\n";
+}
+
+void changeReferenceA(int &c){
+    c = 25;
+    cout<<c<<"\n";
+}
+
 int main(){
     int a = 10;
-    int *pt = &a;
-    int *pt1 = NULL;
-    int **ptr = &pt;
-    cout<<ptr<<"\n";
-    cout<<*pt1<<"\n";  //// Segmentation Error
-    cout<<*pt<<"\n";
+
+    changeValA(a);
+    cout<<a<<"\n";
+
+    int *ptr = &a;
+
+    changePointerA(ptr);
+    cout<<a<<"\n";
+
+    changeReferenceA(a);
+    cout<<a<<"\n";
+
+    // int *pt = &a;
+    // int *pt1 = NULL;
+    // int **ptr = &pt;
+    // cout<<ptr<<"\n";
+    // cout<<*pt1<<"\n";  //// Segmentation Error
+    // cout<<*pt<<"\n";
     return 0;
 }
