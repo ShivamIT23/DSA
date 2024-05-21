a77866e96865fb9f5be0e353937c2741fbe32e99
ShivamIT23 <shivam.gupta.23112003@gmail.com>
2024-05-21 21:58:34 +0530

    Adding code

--- Day_11/TrappingRainwater.cpp
+++ Day_11/TrappingRainwater.cpp
@@ -1,7 +1,37 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    
+void WaterTrapped(int *height, int n)
+{
+    if (n < 4)
+    {
+        return;
+    }
+    int leftMax[20000], rightMax[2000];
+    leftMax[0] = height[0];
+    rightMax[n - 1] = height[n - 1];
+    int water = 0;
+
+    for (int i = 1; i < n; i++)
+    {
+        leftMax[i] = max(leftMax[i - 1], height[i-1]);
+        rightMax[n - (i + 1)] = max(rightMax[n - i], height[n - i]);
+    }
+
+    for(int i=0;i<n;i++){
+        int newWater = min(leftMax[i],rightMax[i]) - height[i];
+        if(newWater>0){
+            water+=newWater;
+        }
+    }
+    cout<<"Trapped Water is = "<<water;
+}
+
+int main()
+{
+    int height[] = {4, 2, 0, 6, 3, 2, 5};
+    int n = sizeof(height) / sizeof(int);
+
+    WaterTrapped(height, n);
     return 0;
 }
