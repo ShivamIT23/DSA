def1e97e3eb869e3607909f5109936c8d1ec3d37
ShivamIT23 <shivam.gupta.23112003@gmail.com>
2024-05-23 21:37:51 +0530

    Adding code

--- Day_12/code.cpp
+++ Day_12/code.cpp
@@ -1,7 +1,56 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    
+void BubbleSort(int *arr, int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        bool swapped = false;
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+        if (!swapped)
+        {
+            break;
+        }
+    }
+}
+
+void SelectionSort(int *arr, int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        int minAt = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[minAt])
+            {
+                minAt = j;
+            }
+        }
+        swap(arr[i], arr[minAt]);
+    }
+}
+
+void DisplayArray(int *arr, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+
+int main()
+{
+    int arr[] = {5, 4, 1, 3, 2};
+    int n = sizeof(arr) / sizeof(int);
+    // BubbleSort(arr, n);
+    SelectionSort(arr, n);
+    DisplayArray(arr, n);
     return 0;
 }
