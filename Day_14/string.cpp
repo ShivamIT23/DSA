f592f5a6d01098985626770b970531a217240afe
ShivamIT23 <shivam.gupta.23112003@gmail.com>
2024-05-28 23:41:54 +0530

    Adding code

--- Day_14/string.cpp
+++ Day_14/string.cpp
@@ -1,8 +1,29 @@
 #include<iostream>
+#include<string>
 #include<cstring>
 using namespace std;
 
 int main(){
-    
+    string str = "hello";
+    cout<<str<<"\n";
+    cout<<str.length()<<"\n";
+    str = "yellop cmdpdmfc";
+    cout<<str<<"\n";
+    cout<<str.length()<<"\n";
+    for(int i =0 ;i<str.length();i++){
+        cout<<str[i]<<" ";
+    }
+    cout<<"\n";
+
+    for (char ch : str){
+        cout<<ch<<"-";
+    }
+    cout<<"\n";
+
+    cout<<str.at(1)<<"\n";
+    cout<<str.substr(3,5)<<"\n";
+    cout<<str.find("m")<<"\n";
+    cout<<str.find("m" , 10)<<"\n";
+
     return 0;
 }
