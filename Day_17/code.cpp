fdc5629ae46dda0457a6ec07a3c1a3d48b6ed0b6
ShivamIT23 <shivam.gupta.23112003@gmail.com>
2024-06-07 12:51:55 +0530

    Adding Code

--- Day_17/code.cpp
+++ Day_17/code.cpp
@@ -1,7 +1,18 @@
 #include<iostream>
 using namespace std;
 
+class Student {
+    string name;
+    float cgpa;
+
+    ///Methods
+    void getPercentage() {
+        cout << (cgpa * 10) << "%\n";
+    }
+};
+
 int main(){
-    
+    Student s1;
+    cout <<sizeof(s1) << "\n";
     return 0;
 }
