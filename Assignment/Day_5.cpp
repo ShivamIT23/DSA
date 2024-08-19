25366d426e5e056de0a60897955629d5ce8a5c60
ShivamIT23 <shivam.gupta.23112003@gmail.com>
2024-08-19 21:23:26 +0530

    Ass 5

---
+++ Assignment/Day_5.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n = 5;
+    int fact =1;
+    for(int i=n;i>=1;i--){
+        fact = fact*i;
+    }
+    cout<<"Factorial of "<<n<<" is "<<fact;
+    return 0;
+}
