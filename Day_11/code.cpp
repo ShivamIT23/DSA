ab96fb4e9677ae1edb0193b60d3c032d7aa223ac
ShivamIT23 <shivam.gupta.23112003@gmail.com>
2024-09-07 16:16:38 +0530

    adding code

--- Day_11/code.cpp
+++ Day_11/code.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 using namespace std;
 
+
+///// SubArrays are small continous order following parts of an array
+///// Subsequence are small [not neccesarily(continous)] order following parts of an array
+//// size of an subarray = n*(n+1) /2
+
 int main()
 {
     int arr[] = {1, 2, 3, 4, 5};
